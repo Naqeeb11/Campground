793447327401a3a928380f79f40fbf2cfca9f766
NAQEEB <naqeeburrahman0@gmail.com>
2019-09-30 18:27:07 +0530

    local port added

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
- app.listen(process.env.PORT, function(){
+ app.listen(900, function(){
      console.log("server started at 900");
  });
